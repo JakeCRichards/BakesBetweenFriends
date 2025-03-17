59f30e340850dd890bd231229acb73a07a13cd26
JakeCRichards <j.c.delaney.92@gmail.com>
2025-03-17 11:45:01 +0000

    DEBUG OFF for Heroku

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,6 +7,5 @@ app_name = 'recipes'
 urlpatterns = [
     path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
     path('', RecipeListView.as_view(), name='recipes'),
-    path('create/', RecipeCreateView.as_view(), name='recipe_create'),
-    
+    path('create/', RecipeCreateView.as_view(), name='recipe_create'),   
 ]
